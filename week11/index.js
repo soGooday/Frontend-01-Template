a55e1293c01420d7089b39b37b38ff15e365bb07
soGooday <630759294@qq.com>
2020-07-08 18:47:08 +0800

    组件化基础 Proxy

---
+++ week11/index.js
@@ -0,0 +1,15 @@
+function init(){
+    var arr = new Array(10000).fill(0);
+    var fragment = document.createDocumentFragment();
+    for (let i = 0; i < array.length; i++) { 
+        for (let j = 0; j < array.length; j++) {
+       
+            
+        }
+        
+    }
+
+
+
+}
+init();
